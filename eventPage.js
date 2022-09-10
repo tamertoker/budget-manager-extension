f5eef95553edaa805d06489eeac29bd1af676f46
tamertoker <tamertoker31@gmail.com>
2022-09-10 18:05:14 +0300

    added exchange rates api

--- eventPage.js
+++ eventPage.js
@@ -7,27 +7,39 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.contextMenus.create(contextMenuItem);
 });
 
+
+var myHeaders = new Headers();
+myHeaders.append("apikey", myapikey); // my api key is hidden
+
+var requestOptions = {
+  method: 'GET',  
+  redirect: 'follow',
+  headers: myHeaders
+};
+
 chrome.contextMenus.onClicked.addListener(function(clickData){
 	if (clickData.menuItemId == "spendMoney" && clickData.selectionText){
-		console.log(clickData.selectionText)
-			if(clickData.selectionText.includes("TL")){
-				clickData.selectionText = clickData.selectionText.replace(/[A-Za-z]/g, "").split('$').join('').split(' ').join('')
-				clickData.selectionText = clickData.selectionText.split('.').join('')
+				price = clickData.selectionText
+			if(price.includes("TL")){
+				price = price.replace(/[A-Za-z]/g, "").split('$').join('').split(' ').join('')
+				price = price.split('.').join('')
 			}
 
-			if (clickData.selectionText.includes("$")){
-				clickData.selectionText = clickData.selectionText.replace(/[A-Za-z]/g, "").split('$').join('').split(' ').join('')
-
-				clickData.selectionText = parseFloat(Number(clickData.selectionText)) * 18.2
+			if (price.includes("$")){
+				price = price.replace(/[A-Za-z]/g, "").split('$').join('').split(' ').join('')
 			}
 
-
-			chrome.storage.sync.get(['total', 'limit'], function(budget){
+			fetch(`https://api.apilayer.com/exchangerates_data/convert?to=TRY&from=USD&amount=${price}`, requestOptions)
+		  .then(response => response.json())
+		  .then(data => {
+		  	price = data.result
+		  				chrome.storage.sync.get(['total', 'limit'], function(budget){
 				let newTotal = 0;
+				console.log(price)
 				if (budget.total){
 					newTotal += parseFloat(budget.total);
 				}
-				newTotal += parseFloat(clickData.selectionText);
+				newTotal += parseFloat(price);
 				chrome.storage.sync.set({'total': newTotal}, function(){
 					if (newTotal >= budget.limit){
 						notifID = "notification-" + new Date().getTime()
@@ -41,9 +53,13 @@ chrome.contextMenus.onClicked.addListener(function(clickData){
 					}
 				});
 			});
-		}
-	});
+		}).catch(error => console.log('error', error));
+	};
+
+		  })
+		  
 
 chrome.storage.onChanged.addListener(function(changes, storageName){
 	chrome.action.setBadgeText({"text":changes.total.newValue.toString()})
 })
+
